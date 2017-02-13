56d4367da7da42ba4fa8d2fa78d1d4a0d437c53b
Mark Gitthens <mgitthens@gmail.com>
2017-02-13 04:14:48 -0600

    Updated Task to closer match the papers description.

--- app/src/main/java/edu/txstate/mjg/ppm/core/Task.java
+++ app/src/main/java/edu/txstate/mjg/ppm/core/Task.java
@@ -1,19 +1,42 @@
 package edu.txstate.mjg.ppm.core;
 
-import android.icu.util.Calendar;
-
 public class Task {
-    private int taskId;
-    private String title;
-    private String description;
-    public boolean completed;
-    public Calendar completionDate; //TODO: Learn how to store date
+
+    /*
+    taskId: the unique integer identifier created for this task
+    title: The official title of the task.
+    Description: information regarding this particular task. (how to complete, required information)
+    state: the status of the current task (not-started / in-progress / completed)
+    creatorID: The creator of this specific task. (changes on modification of the task to the modifying user)
+
+    startTimeStamp: the time when the task was started  (mm/dd/yyyy hh:mm)
+    completionTimeStamp: the time when the task was finished (mm/dd/yyyy hh:mm)
+     */
+
+    enum TaskStates {
+        notStarted,
+        inProgress,
+        completed
+    }
+
+    private int        taskId;
+    private String     title;
+    private String     description;
+    private TaskStates state;
+    private int        creatorId;
+
+    /*
+    TODO: Learn how to store a timestamp properly
+    private time    startTimestamp;
+    private time    completionTimestamp;
+     */
 
     public Task() {
         this.taskId = 0;
         this.title = "Example Task";
         this.description = "This is an example task";
-        this.completed = false;
+        this.creatorId = 0;
+        this.state = TaskStates.notStarted;
     }
 
     public int getTaskId() {
@@ -28,7 +51,9 @@ public class Task {
         return description;
     }
 
-    public boolean getCompletionStatus() {
-        return completed;
-    }
+    public int getCreatorID() { return creatorId; }
+
+    public void setCreatorID(int userID) { creatorId = userID; }
+
+    public TaskStates getCompletionStatus() { return state; }
 }
