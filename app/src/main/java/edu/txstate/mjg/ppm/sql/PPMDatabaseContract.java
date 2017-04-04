49e0cb7ae06ababc6989d9a85be9d45e3606500f
Mark Gitthens <mgitthens@gmail.com>
2017-04-03 19:54:23 -0500

    Fixed functionality of the app due to DB Schema changes.

--- app/src/main/java/edu/txstate/mjg/ppm/sql/PPMDatabaseContract.java
+++ app/src/main/java/edu/txstate/mjg/ppm/sql/PPMDatabaseContract.java
@@ -8,7 +8,6 @@ public class PPMDatabaseContract {
         public static final String COLUMN_PROCESS_TITLE = "title";
         public static final String COLUMN_PROCESS_DESCRIPTION = "description";
         public static final String COLUMN_PROCESS_CATEGORY = "category";
-        public static final String COLUMN_PROCESS_LINKED_TASKS = "linkedTasks";
         public static final String COLUMN_PROCESS_CREATOR_ID = "creatorID";
 
     }
@@ -16,7 +15,6 @@ public class PPMDatabaseContract {
         public static final String TABLE_NAME = "tasks";
         public static final String COLUMN_TASK_TITLE = "title";
         public static final String COLUMN_TASK_DESCRIPTION = "description";
-        public static final String COLUMN_TASK_LINKED_TASKS = "linkedTasks";
         public static final String COLUMN_TASK_CREATOR_ID = "creatorID";
 
     }
