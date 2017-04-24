f5175b149ba18faba10165af78a5b4fbabc2794d
Mark Gitthens <mgitthens@gmail.com>
2017-04-23 19:40:52 -0500

    Created the new user registration activity.

--- app/src/main/java/edu/txstate/mjg/ppm/core/User.java
+++ app/src/main/java/edu/txstate/mjg/ppm/core/User.java
@@ -8,35 +8,43 @@ public class User {
     //TODO: Password NEEDS to be encrypted in someway.
     private int userId;
     private String username;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String password;
     private String email;
 
     public User() {
         userId = 1;
         username = "DefaultUserAccount";
-        firstName = "John";
-        lastName = "lastName";
+        firstname = "John";
+        lastname = "lastName";
         password = "test";
         email = "default@default.com";
     }
 
-    public User(int userId, String username, String password, String firstName, String lastName, String email) {
+    public User(int userId, String username, String password, String firstname, String lastname, String email) {
         this.userId = userId;
         this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.password = password;
         this.email = email;
     }
 
+    public User(String username, String firstname, String lastname, String email, String password){
+        this.userId = 0;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
     public User(JSONObject object) {
         try {
             userId = object.getInt("_id");
             username = object.getString("username");
-            firstName = object.getString("firstName");
-            lastName = object.getString("lastName");
+            firstname = object.getString("firstName");
+            lastname = object.getString("lastName");
             password  = object.getString("password");
             email = object.getString("email");
         } catch (JSONException e) {
@@ -45,15 +53,15 @@ public class User {
     }
     public void setUserId(int userId) { this.userId = userId;}
     public void setUsername(String username) { this.username = username; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setFirstName(String firstName) { this.firstname = firstName; }
+    public void setLastName(String lastName) { this.lastname = lastName; }
     public void setPassword(String password) { this.password = password; }
     public void setEmail(String email) { this.email = email; }
 
     public int getUserId() { return userId; }
     public String getUsername() { return username; }
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
+    public String getFirstName() { return firstname; }
+    public String getLastName() { return lastname; }
     public String getPassword() { return password; }
     public String getEmail() { return email; }
 }
